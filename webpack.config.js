4858f3707fba32c6619a106ac73ce05556c2d959
Matt Wood <designmonkeyboy@gmail.com>
2022-09-11 21:29:37 -0600

    now ready for build

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,43 @@
-const path = require('path');
+const currentTask = process.env.npm_lifecycle_event
+const path = require('path')
+const {CleanWebpackPlugin} = require('clean-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin')
 
 const postCSSPlugins = [
   require('postcss-import'),
   require('postcss-mixins'),
   require('postcss-simple-vars'),
   require('postcss-nested'),
+  // If in the future the creator of the postcss-hexrgba package
+  // releases an update (it is version 2.0.1 as I'm writing this)
+  // then it will likely work with PostCSS V8 so you can uncomment
+  // the line below and also install the package with npm.
+  //require('postcss-hexrgba'),
   require('autoprefixer')
 ]
 
-module.exports = {
+let cssConfig = {
+  test: /\.css$/i,
+  use: ["css-loader?url=false", { loader: "postcss-loader", options: { postcssOptions: { plugins: postCSSPlugins } } }]
+}
+
+let config = {
   entry: './app/assets/scripts/App.js',
-  output: {
-    filename: 'bundled.js', 
+  module: {
+    rules: [
+      cssConfig
+    ]
+  }
+}
+
+if (currentTask == 'dev') {
+  cssConfig.use.unshift('style-loader')
+  config.output = {
+    filename: 'bundled.js',
     path: path.resolve(__dirname, 'app')
-  }, 
-  devServer: {
+  }
+  config.devServer = {
     before: function(app, server) {
       server._watch('./app/**/*.html')
     },
@@ -22,14 +45,24 @@ module.exports = {
     hot: true,
     port: 3000,
     host: '0.0.0.0'
-  },
-  mode: 'development', 
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ['style-loader', 'css-loader?url=false', {loader: 'postcss-loader', options: {postcssOptions: {plugins: postCSSPlugins}}}]
-      }
-    ]
   }
+  config.mode = 'development'
 }
+
+if (currentTask == 'build') {
+  cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+  config.output = {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].js',
+    path: path.resolve(__dirname, 'dist')
+  }
+  config.mode = 'production'
+  config.optimization = {
+    splitChunks: {chunks: 'all'},
+    minimize: true,
+    minimizer: [`...`, new CssMinimizerPlugin()]
+  }
+  config.plugins = [new CleanWebpackPlugin(), new MiniCssExtractPlugin({filename: 'styles.[chunkhash].css'})]
+}
+
+module.exports = config
